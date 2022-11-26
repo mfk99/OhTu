ba450d36871e3c68170c8757392b0039cda0ec9a
Matti <matti.kahkonen99@gmail.com>
2022-11-26 13:07:20 +0200

    kahden saman tuotteen lisääminen lisää koriin kaksi tavaraa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,7 +21,7 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_maara_on_kaksi(self):
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_maara_on_kaksi(self):
         maito = Tuote("Maito", 3)
         porkkana = Tuote("Porkkana", 1)
         self.kori.lisaa_tuote(maito)
@@ -34,3 +34,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(porkkana)
         self.assertEqual(self.kori.hinta(), 4)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_maara_on_kaksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
